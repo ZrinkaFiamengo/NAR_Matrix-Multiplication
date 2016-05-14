beef66db14d5bed17aa908f864d7846f4e8ca2d9
hw-tin <tinmarkoviccs@gmail.com>
2016-05-14 18:42:45 +0200

    Final by Tin

--- NAR_Seminar/ProgramT.cpp
+++ NAR_Seminar/ProgramT.cpp
@@ -33,10 +33,10 @@ void Initiate(int** Array, int* Vector, long* ResultVector, int M, int N){
 		for(int j = 0; j< N; j++){
 			Array[i][j] = rand() % 99 + 1;
 		}
+		ResultVector[i] = 0;
 	}
 	for(int i = 0; i< N; i++){
 		Vector[i] = rand() % 99 + 1;
-		ResultVector[i] = 0;
 	}
 }
 
@@ -53,7 +53,7 @@ void MultiplyByVector(long* ResultVector, int** theArray, int* Vector, int M, in
 	limit = (limit > M) ? M : limit;
 	long temp = 0;
 	for(int i = 0 + offset; i < limit; i++){
-		for(int j = 0; j< N; j++){
+		for(int j = 0; j < N; j++){
 			temp += theArray[i][j] * Vector[j];
 		}
 		ResultVector[i] = temp;
@@ -65,7 +65,7 @@ void VerticalMultiplyByVector(long* ResultVector, int** theArray, int* Vector, i
 	int limit = 0 + offset + length;
 	limit = (limit > N) ? N : limit;
 	long temp = 0;
-	for(int i = 0; i< M; i++){
+	for(int i = 0; i < M; i++){
 		for(int j = 0 + offset; j < limit; j++){
 			temp += theArray[i][j] * Vector[j];
 		}
@@ -94,13 +94,13 @@ int main(int argc, char* argv[])
 	int M = (argc >= 2) ? atoi(argv[1]) : defaultM;
 	int N = (argc >= 3) ? atoi(argv[2]) : defaultN;
 	int Threads = (argc >= 4) ? atoi(argv[3]) : defaultThreads;
-	Threads = (Threads >= 16) ? 16 : Threads;
+	Threads = (Threads >= 32) ? 32 : Threads;
 	cout << "Zeljena matrica: [" << M << "][" << N << "]" << endl;
 	cout << "Broj threadova: " << Threads << endl;
 	
-	double t1, t2;
+	double t1, t2, tt1, tt2;
 	int length, offset;
-	thread p[16];
+	thread p[33];
 	Timer tmr;
  	srand (time(NULL));
 
@@ -109,85 +109,91 @@ int main(int argc, char* argv[])
 	t1 = tmr.elapsed();
 	int** Array = Make2DIntArray(M, N);
 	int* Vector = (int*) malloc( N *sizeof(int));
-	long* ResultVector = (long*) malloc( N *sizeof(long));
-
+	long* ResultVector = (long*) malloc( M *sizeof(long));
 	Initiate(Array, Vector, ResultVector, M, N);
-
 	t2 = tmr.elapsed();
-	cout << "Inicijalizacija (vrijeme): " << (t2-t1)<< endl;
+	cout << "Inicijalizacija (vrijeme): " << (t2-t1) << endl;
+
+// Thread incrementing and looping:
+	for(int t = 1; t< Threads; t = t*2){
+		cout << "========================================= \n" << "Broj niti: " << t << " \n=========================================" << endl;
+		tt1 = tmr.elapsed();
 
 // Mnozenje matrice s vektorom - sekvencijalno
-
-	t1 = tmr.elapsed();
-	MultiplyByVector(ResultVector, Array, Vector, M, N, M, 0);
-	t2 = tmr.elapsed();
-	cout << "Mnozenje matrice s vektorom - sekvencijalno (vrijeme): " << (t2-t1) << endl;
-	cout << "Par rezultata mnozenja: " << ResultVector[1] << ", " << ResultVector[2] << endl;
+		Initiate(Array, Vector, ResultVector, M, N);
+		t1 = tmr.elapsed();
+		MultiplyByVector(ResultVector, Array, Vector, M, N, M, 0);
+		t2 = tmr.elapsed();
+		cout << "Horizontalno - sekvencijalno (vrijeme): " << (t2-t1) << endl;
+		cout << "Par rezultata mnozenja: 					" << ResultVector[1] << ", " << ResultVector[2] << endl;
 
 // Mnozenje matrice s vektorom - paralelno
 
-	Initiate(Array, Vector, ResultVector, M, N);
-	length = M / Threads;
-	t1 = tmr.elapsed();
-	for( int z = 0; z < Threads; z++){
-		offset = z * length;
-		p[z] = std::thread(MultiplyByVector, ResultVector, Array, Vector, M, N, length, offset);
-	}
-	for( int z = 0; z < Threads; z++){
-		p[z].join();
-	}
-	t2 = tmr.elapsed();
-	cout << "Mnozenje matrice s vektorom - paralelno (vrijeme): " << (t2-t1) << endl;
-	cout << "Par rezultata mnozenja: " << ResultVector[1] << ", " << ResultVector[2] << endl;
+		Initiate(Array, Vector, ResultVector, M, N);
+		length = M / t;
+		t1 = tmr.elapsed();
+		for( int z = 0; z < t; z++){
+			offset = z * length;
+			p[z] = std::thread(MultiplyByVector, ResultVector, Array, Vector, M, N, length, offset);
+			p[z].join();
+		}
+		for( int z = 0; z < t; z++){
+		}
+		t2 = tmr.elapsed();
+		cout << "Horizontalno - paralelno (vrijeme): " << (t2-t1) << endl;
+		cout << "Par rezultata mnozenja: 					" << ResultVector[1] << ", " << ResultVector[2] << endl;
 
 // Mnozenje matrice s vektorom - vertikalno - sekvencijalno
 
-	Initiate(Array, Vector, ResultVector, M, N);
-	t1 = tmr.elapsed();
-	VerticalMultiplyByVector(ResultVector, Array, Vector, M, N, N, 0);
-	t2 = tmr.elapsed();
-	cout << "Mnozenje matrice s vektorom - vertikalno - sekvencijalno (vrijeme): " << (t2-t1) << endl;
-	cout << "Par rezultata mnozenja: " << ResultVector[1] << ", " << ResultVector[2] << endl;
+		Initiate(Array, Vector, ResultVector, M, N);
+		t1 = tmr.elapsed();
+		VerticalMultiplyByVector(ResultVector, Array, Vector, M, N, N, 0);
+		t2 = tmr.elapsed();
+		cout << "Vertikalno - sekvencijalno (vrijeme): 	" << (t2-t1) << endl;
+		cout << "Par rezultata mnozenja: 					" << ResultVector[1] << ", " << ResultVector[2] << endl;
 
 // Mnozenje matrice s vektorom - vertikalno - paralelno
 
-	Initiate(Array, Vector, ResultVector, M, N);
-	length = N / Threads;
-	t1 = tmr.elapsed();
-	for( int z = 0; z < Threads; z++){
-		offset = z * length;
-		p[z] = std::thread(VerticalMultiplyByVector, ResultVector, Array, Vector, M, N, length, offset);
-	}
-	for( int z = 0; z < Threads; z++){
-		p[z].join();
-	}
-	t2 = tmr.elapsed();
-	cout << "Mnozenje matrice s vektorom - vertikalno - paralelno (vrijeme): " << (t2-t1) << endl;
-	cout << "Par rezultata mnozenja: " << ResultVector[1] << ", " << ResultVector[2] << endl;
+		Initiate(Array, Vector, ResultVector, M, N);
+		length = N / t;
+		t1 = tmr.elapsed();
+		for( int z = 0; z < t; z++){
+			offset = z * length;
+			p[z] = std::thread(VerticalMultiplyByVector, ResultVector, Array, Vector, M, N, length, offset);
+		}
+		for( int z = 0; z < t; z++){
+			p[z].join();
+		}
+		t2 = tmr.elapsed();
+		cout << "Vertikalno - paralelno (vrijeme): 	" << (t2-t1) << endl;
+		cout << "Par rezultata mnozenja: 					" << ResultVector[1] << ", " << ResultVector[2] << endl;
 
 // Mnozenje matrice s vektorom - vertikalno - sekvencijalno - glupo
 
-	Initiate(Array, Vector, ResultVector, M, N);
-	t1 = tmr.elapsed();
-	DumbVerticalMultiplyByVector(ResultVector, Array, Vector, M, N, N, 0);
-	t2 = tmr.elapsed();
-	cout << "Mnozenje matrice s vektorom - vertikalno - sekvencijalno - glupo (vrijeme): " << (t2-t1) << endl;
-	cout << "Par rezultata mnozenja: " << ResultVector[1] << ", " << ResultVector[2] << endl;
+		Initiate(Array, Vector, ResultVector, M, N);
+		t1 = tmr.elapsed();
+		DumbVerticalMultiplyByVector(ResultVector, Array, Vector, M, N, N, 0);
+		t2 = tmr.elapsed();
+		cout << "Ne-ef. vertikalno - sekv (vrijeme): " << (t2-t1) << endl;
+		cout << "Par rezultata mnozenja: 					" << ResultVector[1] << ", " << ResultVector[2] << endl;
 
 // Mnozenje matrice s vektorom - vertikalno - paralelno
 
-	Initiate(Array, Vector, ResultVector, M, N);
-	length = N / Threads;
-	t1 = tmr.elapsed();
-	for( int z = 0; z < Threads; z++){
-		offset = z * length;
-		p[z] = std::thread(DumbVerticalMultiplyByVector, ResultVector, Array, Vector, M, N, length, offset);
-	}
-	for( int z = 0; z < Threads; z++){
-		p[z].join();
-	}
-	t2 = tmr.elapsed();
-	cout << "Mnozenje matrice s vektorom - vertikalno - paralelno - glupo (vrijeme): " << (t2-t1) << endl;
-	cout << "Par rezultata mnozenja: " << ResultVector[1] << ", " << ResultVector[2] << endl;
+		Initiate(Array, Vector, ResultVector, M, N);
+		length = N / t;
+		t1 = tmr.elapsed();
+		for( int z = 0; z < t; z++){
+			offset = z * length;
+			p[z] = std::thread(DumbVerticalMultiplyByVector, ResultVector, Array, Vector, M, N, length, offset);
+		}
+		for( int z = 0; z < t; z++){
+			p[z].join();
+		}
+		t2 = tmr.elapsed();
+		cout << "Ne-ef. vertikalno - prll (vrijeme): " << (t2-t1) << endl;
+		cout << "Par rezultata mnozenja: 					" << ResultVector[1] << ", " << ResultVector[2] << endl;
 
+		tt2 = tmr.elapsed();
+		cout << "========================================= \n" << "Za " << t << " niti potrebno je totalno " << tt2 - tt1 << " \n=========================================" << endl;
+	}
 }
